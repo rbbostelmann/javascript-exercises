62b9df12efab66eeca0bf8a99517d52932801de9
rbbostelmann <rodrigobostelmann@gmail.com>
2022-03-29 12:21:43 -0300

    Exercise 10 solved

---
+++ fibonacci.js
@@ -0,0 +1,14 @@
+function fibonacci(n) {
+    if (n === 0) {
+        return 0
+    } else if (n < 0) {
+        return "OOPS"
+
+    } else if (n == 1 || n == 2) {
+        return 1
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2)
+};
+
+// Do not edit below this line
+module.exports = fibonacci;
